80096b592708ff892685ec797b518cd43daec432
zhouqiang <zhouqiang@gejiantech.onaliyun.com>
2021-09-06 14:12:15 +0800

    buy hero funcation

--- src/main/java/com/gejian/pixel/service/BuyHeroService.java
+++ src/main/java/com/gejian/pixel/service/BuyHeroService.java
@@ -3,9 +3,12 @@ package com.gejian.pixel.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.gejian.pixel.entity.BuyHero;
 
+import javax.script.ScriptException;
+
 /**
  *  Auto created by codeAppend plugin
  */
 public interface BuyHeroService extends IService<BuyHero> {
 
+	double calculation(Integer id, Integer count) throws ScriptException;
 }
