b824668a6afb5adedb900c49b8f5335182ca79cb
ljb <serve@tom.com>
2021-09-08 09:37:37 +0800

    test

--- src/test/java/WebSocketClientTests.java
+++ src/test/java/WebSocketClientTests.java
@@ -1,14 +1,20 @@
+import cn.hutool.core.text.StrFormatter;
+import cn.hutool.core.util.NumberUtil;
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.crypto.SecureUtil;
 import com.gejian.pixel.constants.CommandConstants;
+import com.gejian.pixel.enums.ErrorEnum;
 import com.gejian.pixel.proto.CommLoginRequestProtobuf;
 import com.gejian.pixel.proto.CommLoginResponseProtobuf;
 import com.gejian.pixel.proto.MessageBaseProtobuf;
 import com.google.protobuf.InvalidProtocolBufferException;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.codec.digest.DigestUtils;
 
 import javax.websocket.*;
 import java.net.URI;
 import java.nio.ByteBuffer;
+import java.security.MessageDigest;
 
 /**
  * @author ZhouQiang
@@ -42,14 +48,21 @@ public class WebSocketClientTests {
 	public static void main(String[] args) throws Exception {
 		WebSocketContainer container = ContainerProvider.getWebSocketContainer();
 		String uri = "ws://127.0.0.1:19999/ws";
+		//String uri = "ws://110.40.133.173:32011/ws";
 		Session session = container.connectToServer(WebSocketClientTests.class, URI.create(uri));
 
 		CommLoginRequestProtobuf.CommLoginRequest request = CommLoginRequestProtobuf.CommLoginRequest.newBuilder()
-				.setIdentifier("104")
+				.setIdentifier("")
 				.setVersion(11)
-				.setData("1630488085")
-				.setCipher("ff56ce1dd084f89ea67c4db077dc21fa0c7d4d2d")
+				.setData("1630986780")
+				.setCipher("DBDE9269F4E47C0FC8B111854B43C551C8A58974")
 				.build();
+		/*CommLoginRequestProtobuf.CommLoginRequest request = CommLoginRequestProtobuf.CommLoginRequest.newBuilder()
+				.setIdentifier("28")
+				.setVersion(11)
+				.setData("1630997451")
+				.setCipher("D14B356084853978DB05326E6AFFFDA1EFD76EBB")
+				.build();*/
 		byte[] bytes = MessageBaseProtobuf.MessageBase.newBuilder()
 				.setName(CommandConstants.LOGIN_REQUEST)
 				.setData(request.toByteString())
@@ -61,6 +74,20 @@ public class WebSocketClientTests {
 						.sendBinary(ByteBuffer.wrap(bytes),true);
 		Thread.sleep(500000);
 
+	}
+
+	public static void main1(String[] args) {
+		/*String s = StrFormatter.format("{} {}  {}   {}",
+				"1630986780",
+				11,
+				"",
+				NumberUtil.parseLong("1630986780") * 11);*/
+		String s = StrFormatter.format("{} {}  {}   {}",
+				"1630986780",
+				11,
+				"25",
+				NumberUtil.parseLong("1630986780") * 11);
+		System.out.println("SecureUtil.sha1(s) = " + SecureUtil.sha1(s).toUpperCase());
 	}
 
 }
